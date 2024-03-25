e1768cc9f5172a7346e3fe2e7d93c2be208675e3
SaiNandhan08 <114205367+SaiNandhan08@users.noreply.github.com>
2024-03-24 22:25:19 -0500

    Add files via upload

---
+++ Path Visualiser/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Pathapp from './Path_app/Path_app';
+
+function App() {
+  return (
+    <div className="App">
+      <Pathapp></Pathapp>
+    </div>
+  );
+}
+
+export default App;
